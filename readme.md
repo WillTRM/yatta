d6e882be15515f90f96cc97978960371e8fabc37
WillTRM <willtrm111@gmail.com>
2025-09-22 14:19:52 -0500

    added cmake and added options

--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-Yet Another Terminal Todo App
+# Yet Another Terminal Todo App
+
+I believe this requires SQLite3 to be installed on your system.
+Results may vary.
+I'm a professional.
+
+**Usage**
+---
+Run `cmake build/`
+Run `make`
+Run program with `./yatta`
+
+https://github.com/ArthurSonzogni/FTXUI
+look at this
--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,14 @@
 */
 static int callback(void *unused, int count, char **data, char **columns)
 {
-   std::cout << data[0] << ". " << data[1] << " (" << data[3] << ")\n";
+   std::cout << data[0] << ". " << data[1] << " (" << data[2] << ")\n";
    return 0;
 }
-
 
 void reset_table(sqlite3 *db, char *error_msg, int dc)
 {
    const char* sql_drop = "DROP TABLE if EXISTS tasks";
-   const char* sql_create = "CREATE TABLE tasks (NAME TEXT, COMPLETED BOOL, DUE DATETIME)";
+   const char* sql_create = "CREATE TABLE tasks (NAME TEXT, DUE TEXT)";
    // executes the passed SQL statement and for every result sow calls 
    dc = sqlite3_exec(db, sql_drop, callback, 0, &error_msg);
    dc = sqlite3_exec(db, sql_create, callback, 0, &error_msg);
@@ -28,7 +27,7 @@ void reset_table(sqlite3 *db, char *error_msg, int dc)
       std::cout << "SQL error: " << error_msg << "\n";
       sqlite3_free(error_msg);
    } else {
-      std::cout << "Table created successfully\n";
+      std::cout << "Tasks cleared successfully\n";
    }
 }
 
@@ -36,7 +35,7 @@ void reset_table(sqlite3 *db, char *error_msg, int dc)
 void add_to_table(sqlite3 *db, char *error_msg, int dc, std::string task, std::string due_date)
 {
    std::stringstream sql_insert;
-   sql_insert << "INSERT INTO tasks (NAME, COMPLETED, DUE) VALUES ('" << task << "', false, '" << due_date << "')";
+   sql_insert << "INSERT INTO tasks (NAME, DUE) VALUES ('" << task << "', '" << due_date << "')";
    dc = sqlite3_exec(db, sql_insert.str().c_str(), callback, 0, &error_msg);
 
    if(dc != SQLITE_OK){
@@ -45,6 +44,13 @@ void add_to_table(sqlite3 *db, char *error_msg, int dc, std::string task, std::s
    } else {
       std::cout << "Task added successfully\n";
    }
+}
+
+void remove_from_table(sqlite3 *db, char *error_msg, int dc, int id_to_remove)
+{
+   std::stringstream sql_remove;
+   sql_remove << "DELETE FROM tasks WHERE rowid = " << id_to_remove;
+   sqlite3_exec(db, sql_remove.str().c_str(), callback, 0, &error_msg);
 }
 
 void show_all_tasks(sqlite3 *db, char *error_msg, int dc)
@@ -63,35 +69,37 @@ int main()
       std::cout << "Cannot open database\n";
       exit(1);
    } else {
-      std::cout << "Database opened\n";
+      std::cout << "Database opened\n\n";
    }
 
-   // reset_table(db, error_msg, dc);
-   // add_to_table(db, error_msg, dc, "kill everyone", "20250922 11:00:00 AM");
    int done = 0;
    while (not done) {
       std::cout << "Select an option:\n";
       std::cout << "1. Show all tasks\n";
       std::cout << "2. Add a task\n";
-      std::cout << "3. Exit\n";
+      std::cout << "3. Complete a task\n";
+      std::cout << "4. Clear all tasks\n";
+      std::cout << "5. Exit\n";
       int choice;
       std::cin >> choice;
 
       std::string task_name;
       std::string due_date;
+      int task_num;
 
       switch (choice) {
          case 1:
             std::cout << "\n";
             show_all_tasks(db, error_msg, dc);
+            std::cout << "\n";
             break;
 
          case 2:
             
-            std::cout << "Enter name of task: ";
+            std::cout << "\nEnter name of task: ";
             std::cin.ignore();
             std::getline(std::cin, task_name);
-            std::cout << "Enter when task should be done by (YYYYMMDD HH:MM:SS tt): ";
+            std::cout << "Enter when task should be done by: ";
             std::cin.ignore();
             std::getline(std::cin, due_date);
             add_to_table(db, error_msg, dc, task_name, due_date);
@@ -99,6 +107,16 @@ int main()
             break;
 
          case 3:
+            std::cout << "Enter ID of task to complete: ";
+            std::cin >> task_num;
+            remove_from_table(db, error_msg, dc, task_num);
+            break;
+
+         case 4:
+            reset_table(db, error_msg, dc);
+            break;
+
+         case 5:
          done = 1;
             break;
       }
